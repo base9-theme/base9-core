9729c354b2ed9f501d4bdfb78397a7f04d97d925
Forrest Li <lijiaqigreat@gmail.com>
2022-05-23 21:58:51 -0700

    Use new semantic.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Base9 Core Library
+It has the source of truth for base9 theme generation.
+
+Minor version will change when semantics change.
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,19 @@
 import Color from 'color';
 import { Dictionary } from 'lodash';
+export declare type ColorPalette = [
+    Color,
+    Color,
+    Color,
+    Color,
+    Color,
+    Color,
+    Color,
+    Color,
+    Color
+];
+export declare const PALETTE_REGEX: RegExp;
+export declare function toPaletteString(colors: ColorPalette): string;
+export declare function fromPaletteString(palette: string): ColorPalette | undefined;
 declare class ColorWithFormat {
     color: Color;
     constructor(color: Color);
@@ -15,17 +29,21 @@ declare class ColorWithFormat {
     get fracg(): string;
     get fracb(): string;
 }
-declare type Semantic = Dictionary<string | undefined>[];
+declare type Semantic = Dictionary<string | Semantic>;
 declare type Config = {
-    mapping: Semantic;
-    r1: number;
-    r2: number;
-    todoSize: number;
+    semantic: Semantic;
+    shades: {
+        p10: number;
+        p25: number;
+        p50: number;
+        p75: number;
+        p125: number;
+    };
 };
 export declare const defaultConfig: Config;
-export declare function getNamedColors(palette: Color[], cfg?: Config): NamedColors;
 export declare type NamedColors = {
-    [k: string]: ColorWithFormat | NamedColors | undefined;
+    [k: string]: ColorWithFormat | NamedColors;
 };
+export declare function getNamedColors(palette: Color[], cfg?: Config): NamedColors;
 export declare function render(template: string, cs: Color[], cfg?: Config): string;
 export {};
